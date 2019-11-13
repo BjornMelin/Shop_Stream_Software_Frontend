0b607bd73791bbfa3c1e7a670b1a06983306727f
Bjorn Melin <melinb@augsburg.edu>
2019-11-12 22:42:40 -0600

    Added Backgrounds and AppBars to pages

--- src/containers/production/Production.js
+++ src/containers/production/Production.js
@@ -3,18 +3,35 @@ import React from 'react';
 import MainAppBar from '../AppBar/MainAppBar'
 
 
+import Image from '../images/bill-oxford--fGqsewtsJY-unsplash.png'; 
+import { makeStyles } from '@material-ui/core/styles';
 
+
+const useStyles = makeStyles(theme => ({
+    '@global': {
+      body: {
+        backgroundHeight:'100%',
+        backgroundWidth:"100%",
+        textAlign: 'center',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundImage: `url(${Image})`,
+        backgroundPosition: "top",
+        
+      },
+    },
+}));
 
 
 export default function Production() {
+    const classes = useStyles();
 
     return (
     <div>
         <MainAppBar>
         </MainAppBar>
-        {/* <List>{mainListItems}</List>
-        <Divider />
-        <List>{secondaryListItems}</List> */}
+
     </div>
     );
 }
