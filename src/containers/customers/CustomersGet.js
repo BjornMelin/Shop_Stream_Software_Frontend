dbeb54c858a08ff6d5df583406506f169b957729
Bjorn Melin <melinb@augsburg.edu>
2019-12-03 17:40:51 -0600

    GET For Customers Is Working & Displaying In Grid List

--- src/containers/customers/CustomersGet.js
+++ src/containers/customers/CustomersGet.js
@@ -5,56 +5,53 @@ import { makeStyles } from '@material-ui/core/styles';
 // import MenuDropdown from '../AppBar/MenuDropdown'
 import superagent from 'superagent';
 import Button from '@material-ui/core/Button';
-import CustTable from './CustTable';
-import Table from '@material-ui/core/Table';
 import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
 import ReactJson from 'react-json-view'
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { DataGrid } from 'tubular-react';
+import { ColumnModel } from 'tubular-common';
+import AddIcon from "@material-ui/icons/Add";
+import IconButton from "@material-ui/core/IconButton";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
 
 
 
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   root: {
-    width: '100%',
-  },
-  paper: {
     width: '100%',
     overflowX: 'auto',
   },
-  table: {
-    minWidth: 650,
+  submit: {
+    margin: theme.spacing(3, 0, 2),
   },
-  cardCategoryWhite: {
-    "&,& a,& a:hover,& a:focus": {
-      color: "rgba(255,255,255,.62)",
-      margin: "0",
-      fontSize: "14px",
-      marginTop: "0",
-      marginBottom: "0"
-    },
-    "& a,& a:hover,& a:focus": {
-      color: "#FFFFFF"
-    }
+  // dataGrid: {
+  //   paddingTop: theme.spacing(1),
+  //   paddingLeft: theme.spacing(1),
+  //   paddingRight: theme.spacing(1),
+  //   paddingBottom: theme.spacing(1),
+  //   display: 'flex',
+  //   flexDirection: 'column',
+  //   width: '100%',
+  //   alignItems: 'center',
+  //   backgroundColor: theme.palette.common.white,
+  // },
+  paper: {
+    marginTop: theme.spacing(12),
+    marginLeft: theme.spacing(28),
+    display: 'flex',
+    flexDirection: 'column',
+    width: '75%',
+    alignItems: 'center',
+    backgroundColor: theme.palette.common.white,
   },
-  cardTitleWhite: {
-    color: "#FFFFFF",
-    marginTop: "0px",
-    minHeight: "auto",
-    fontWeight: "300",
-    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
-    marginBottom: "3px",
-    textDecoration: "none",
-    "& small": {
-      color: "#777",
-      fontSize: "65%",
-      fontWeight: "400",
-      lineHeight: "1"
-    }
-  },
-});
+}));
 
 
 
@@ -69,110 +66,61 @@ export default function CustomersView() {
     const values = {
       customers: []
     }
+
+    const columns = [
+      new ColumnModel('nameFirst'),
+      new ColumnModel('nameLast'),
+      new ColumnModel('companyName'),
+      new ColumnModel('email'),
+      new ColumnModel('phoneNum')
+    ];
+
+
     
     async function getCustData() {
 
         const result = await superagent.get('http://127.0.0.1:4000/api/getData');
-        alert("SUCCESS: " + (JSON.parse(result.text)));
+        // alert("SUCCESS: " + (JSON.parse(result.text)));
 
         const data = JSON.parse(result.text);
         var i;
         for (i = 0; i < data.length; i++)
         {
-          values.customers.push((data[i]))
+          delete(data[i]._id);
+          delete(data[i].__v);
+          values.customers.push((data[i]));
         }
-
         // var allCustomers = [];
         // allCustomers.push(JSON.stringify(result.body));
         // alert("Here: " + allCustomers);
-
         // const customersAll = 
         // values.customers.push(JSON.stringify(result.body));
-       
         // alert("Customers: " + customersAll);
         // alert("Customers: " + values.customers);
-        
-
     }
 
 
     return (
-      <div marginTop={15}>
-      <Grid container spacing={2}>
-      <Grid item xs={12} sm={12} md={12}>
-        <Button onClick={getCustData}>
-          View Customers
+      <Container component="main" maxWidth="s">
+
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Button            
+            variant="outlined"
+            color="primary"
+            onClick={getCustData} 
+            > 
+        Click Here Then Select Any Option In Grid To Load Customers
         </Button>
-        <ReactJson src={values.customers} onEdit={true}/>
-        </Grid>
+      <Grid container >
+        <DataGrid 
+            columns={columns}
+            dataSource={values.customers}
+            gridName='Grid'
+            />
       </Grid>
       </div>
+      </Container>
     )
 }
-        // <div marginTop={15}>
-        //     <Grid container spacing={2}>
-        //         <Grid item xs={12} sm={12} md={12}>
-        //           {/* <Button color="primary" onClick={getCustData}>View Customers</Button> */}
-        //           <Table {customers.map((data, i) =>  {
-        //             return (
-
-        //             )
-        //           })}
-                  // {/* <Card> */}
-                    // {/* <CardHeader color="primary">
-                      // <h4 className={classes.cardTitleWhite}>Customers List</h4>
-                      // <p className={classes.cardCategoryWhite}>
-                        // {/*Here is a subtitle for this table*/}
-                      // {/* </p> */}
-                    // {/* </CardHeader> */} 
-                    // {/* <CardContent>
-                      // <Table
-                      //   tableHeaderColor="primary"
-                      //   tableHead={["First Name", "Last Name", "Company Name",
-                      //               "Email", "Phone Number"]}
-                      //   tableData={result.body}
-                      // />
-                    // </CardContent> */}
-                    // {/* <Card> */}
-                      // {/* <Button color="primary">Update Customer</Button> */}
-                    // {/* </Card> */}
-                  // {/* </Card> */}
-        //         </Grid>
-        //       </Grid>
-        // </div>
-    // );
-// }
-
-        // <span>
-        //     {this.state.result.map(function(object) {
-        //       return (
-        //         <li key={object.id} data={object} />
-        //       );
-        //     })}
-        //     </span>
-
-
-
-    /*
-    .send({}) // sends a JSON post body
-    .set('accept', 'json')
-    .end((err, res) => {
-      // Calling the end function will send the request
-      alert("SUCCESS: " + JSON.stringify(res));
-  });
-  */
-//  alert("SUCCESS: " + JSON.stringify(result));
-//   console.log(result);
-
-//   return (
-//     <span>
-//     // This will go through all the elements in arrayFromJson and
-//     // render each one as a <SomeComponent /> with data from the object
-//     {this.state.arrayFromJson.map(function(object) {
-//       return (
-//         <TextView key={object.id} data={object} />
-//       );
-//     })}
-//   </span>
-//         // console.log(result)
-    // );
+      
--- src/containers/customers/CustomersView.js
+++ src/containers/customers/CustomersView.js
@@ -3,11 +3,7 @@ import React from 'react';
 import Image from '../images/bill-oxford--fGqsewtsJY-unsplash.png'; 
 import { makeStyles } from '@material-ui/core/styles';
 import MenuDropdown from '../AppBar/MenuDropdown'
-import superagent from 'superagent';
 import CustomersGet from './CustomersGet'
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import CustTable from './CustTable';
 
 
 
@@ -24,46 +20,18 @@ const useStyles = makeStyles(theme => ({
       
     },
   },
-  paper: {
-    marginTop: theme.spacing(18),
-    marginLeft: theme.spacing(28),
-    display: 'flex',
-    flexDirection: 'column',
-    width: '75%',
-    alignItems: 'center',
-    backgroundColor: theme.palette.common.white,
-  },
 }));
 
 
 export default function CustomersView() {
 
-// export default async function CustomersView() {
-
     const classes = useStyles();
 
-    // const result = await superagent.get('http://127.0.0.1:4000/api/getData');
-    /*
-    .send({}) // sends a JSON post body
-    .set('accept', 'json')
-    .end((err, res) => {
-      // Calling the end function will send the request
-      alert("SUCCESS: " + JSON.stringify(res));
-  });
-  */
-//  alert("SUCCESS: " + JSON.stringify(result));
-//   console.log(result);
-  
 
     return (
     <div className={classes.root}>
         <MenuDropdown />
-
-        <Grid item xs={24} >
-          <Paper className={classes.paper}>
-            <CustomersGet />
-          </Paper>
-        </Grid>    
+        <CustomersGet />
     </div>
     );
 }
